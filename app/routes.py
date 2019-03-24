b0d6e3b91f9254ecc963049751be6da4f5e76243
Jayna Kehres <jayna822@gmail.com>
2019-03-24 13:23:56 -0500

    adding initial files

---
+++ app/routes.py
@@ -0,0 +1,28 @@
+from flask import render_template
+from app import app
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title=None, user=user, posts=posts)
+
+@app.route('/aboutme')
+def aboutMe():
+    return render_template('aboutme.html', title='About Me!')
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign in', form=form)
